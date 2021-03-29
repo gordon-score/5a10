2ea4572ce7a10500ca5aaf90ada0281f58649257
gordon-score <81543771+gordon-score@users.noreply.github.com>
2021-03-29 10:41:27 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Cube = 0
+input.onGesture(Gesture.Shake, function () {
+    Cube = randint(1, 6)
+    if (Cube == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Cube == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (Cube == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (Cube == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Cube == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
